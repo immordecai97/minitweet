e53988ebcf8bd98ed574ac38bf31b7da56f1f550
immordecai97 <carlosscripto@gmail.com>
2024-11-11 18:11:46 -0500

    Refactor PrivateChatView.vue: Comment out viewport height utility and implement new height class for improved layout

--- src/views/PrivateChatView.vue
+++ src/views/PrivateChatView.vue
@@ -69,7 +69,8 @@ onUnmounted(() => {
 </script>
 
 <template>
-    <div class="grid grid-rows-[auto_1fr] h-[calc(100vh-104px)]">
+    <!-- <div class="grid grid-rows-[auto_1fr] h-[calc(100vh-104px)]"> -->
+    <div class="grid grid-rows-[auto_1fr] h-cal">
         <!-- header -->
         <ContainerComp class="max-w-96 p-4 bg-black flex items-center gap-4 shadow">
             <ProfilePhotoComp :src="userToChat?.photoURL" :alt="userToChat?.name" width="w-12" height="h-12" />
@@ -120,3 +121,9 @@ onUnmounted(() => {
         </ContainerComp>
     </Teleport>
 </template>
+
+<style scoped>
+.h-cal{
+    height: calc(100vh - 104px);
+}
+</style>
